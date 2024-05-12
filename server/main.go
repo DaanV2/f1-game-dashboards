2ab52d0d6a767e28f2c1fa6ab101491fdba023ab
DaanV2 <daanverstraten@hotmail.com>
2024-05-12 15:21:44 +0200

    Adding initial setup

---
+++ server/main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"github.com/DaanV2/f1-game-dashboards/server/cmd"
+
+	_ "go.uber.org/automaxprocs"
+)
+
+func main() {
+	cmd.Execute()
+}
